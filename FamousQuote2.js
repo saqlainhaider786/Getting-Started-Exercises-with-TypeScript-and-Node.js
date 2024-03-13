a320869b6345dd7f0669bf39a0fc7ce90beb91d8
Syed Saqlain Haider <saqlainhaider786985@gmail.com>
2024-03-13 15:36:16 +0500

    Striping white spaces.

--- FamousQuote2.js
+++ FamousQuote2.js
@@ -1,4 +1,4 @@
-let quote = "Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains."
-let quoter = "Bill Gates";
+let famous_person = "Bill Gates";
+let message =`${famous_person} once said,"Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.`
 
-console.log(`${quoter} once said, "${quote}"`);
+console.log(message);
---
+++ StrippingNames.js
@@ -0,0 +1,4 @@
+let person_name="\tSyed Saqlain Haider\n";
+
+console.log(person_name);
+console.log(person_name.trim())
