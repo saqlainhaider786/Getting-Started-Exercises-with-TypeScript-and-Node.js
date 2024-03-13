521f69291b8ed0b6b6dfb220bd417cb0e7dcb9b4
Syed Saqlain Haider <saqlainhaider786985@gmail.com>
2024-03-13 15:29:16 +0500

    Previous task with usage of variables

---
+++ FamousQuote2.js
@@ -0,0 +1,4 @@
+let quote = "Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains."
+let quoter = "Bill Gates";
+
+console.log(`${quoter} once said, "${quote}"`);
