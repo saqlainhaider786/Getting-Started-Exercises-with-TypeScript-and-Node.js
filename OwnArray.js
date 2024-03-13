cd9c77c487d706add0e5bf8ac5291a9d0b660041
Syed Saqlain Haider <saqlainhaider786985@gmail.com>
2024-03-13 16:22:32 +0500

    A series of statements.

---
+++ OwnArray.js
@@ -0,0 +1,7 @@
+let favTransportation="Motorcycle";
+let brand="Suzuki"
+let statements=["I would like to drive","I would like to be an owner of","I would like to go on long drive on"];
+
+for(let statement of statements){
+    console.log(`${statement} ${brand} ${favTransportation}`)
+}
