8f4016b4a687552309fb54ca01f53975b704c967
Syed Saqlain Haider <saqlainhaider786985@gmail.com>
2024-03-13 15:26:03 +0500

    Famous person's quote is added

--- FamousQuote.js
+++
@@ -1,4 +0,0 @@
-let quote = "Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains."
-let quoter = "Bill Gates";
-
-console.log(`${quoter} once said, "${quote}"`);
