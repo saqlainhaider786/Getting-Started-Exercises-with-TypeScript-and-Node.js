b4280cf3318a8c073bfed8b13945cebdd266d277
Syed Saqlain Haider <saqlainhaider786985@gmail.com>
2024-03-13 16:31:03 +0500

    Invitation message for my most favourite persons is set!.

---
+++ GuestList.js
@@ -0,0 +1,5 @@
+let guests=["Mehdi","Syed Ali Khamenei","Haj Qasim Solemani"];
+
+for(let guest of guests){
+    console.log(`Dear ${guest}, it is pleasure for me that I am inviting you to dinner at my home, Kindly accept my invitation and make my home blessed...\nThank you.!`);
+}
