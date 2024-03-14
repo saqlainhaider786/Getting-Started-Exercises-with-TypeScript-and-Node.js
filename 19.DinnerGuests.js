7674872a200a1d958fa17cc6db175e366e29c0ea
Syed Saqlain Haider <saqlainhaider786985@gmail.com>
2024-03-14 15:46:44 +0500

    Number of guests on dinner

---
+++ 19.DinnerGuests.js
@@ -0,0 +1,5 @@
+let guests=["Ali","Mehdi","Hassan","Syed Ali Khamenei","Haj Qasim Solemani","Haider"];
+
+let no_of_guests=guests.length
+
+console.log(`I am inviting ${no_of_guests} people to dinner`)
