797e61dece648d9b959660f829d7a00c2e42bce4
Syed Saqlain Haider <saqlainhaider786985@gmail.com>
2024-03-14 14:32:20 +0500

    Unavailable guest is replaces

---
+++ ChangingGuestList.js
@@ -0,0 +1,6 @@
+let guests=["Mehdi","Syed Ali Khamenei","Haj Qasim Solemani"];
+// I heard that Haj Qasim Solemani cannot make the dinner
+guests[2]="Hassan Nasrullah";
+for(let guest of guests){
+    console.log(`Dear ${guest}, it is pleasure for me that I am inviting you to dinner at my home, Kindly accept my invitation and make my home blessed...\nThank you.!`);
+}
