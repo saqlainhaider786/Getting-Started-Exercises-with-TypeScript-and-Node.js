144bb75165e9c09bee8ada376c608e711c9b296a
Syed Saqlain Haider <saqlainhaider786985@gmail.com>
2024-03-14 16:21:47 +0500

    Favourite Fruits list.

---
+++ FavouriteFruit.js
@@ -0,0 +1,9 @@
+let favorite_fruits=["Orange","Banana","Apple","Pine Apple","Pears"];
+
+let certainFruit="Apple";
+
+for(let fruit of favorite_fruits){
+    if(fruit===certainFruit){
+        console.log(`I really like ${fruit}`)
+    }
+}
