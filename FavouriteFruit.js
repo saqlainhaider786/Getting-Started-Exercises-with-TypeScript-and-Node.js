bdd08bcca6a02c64b8ec9471c5e917acda85e23a
Syed Saqlain Haider <saqlainhaider786985@gmail.com>
2024-03-14 16:26:05 +0500

    Favourite Fruits list.

--- FavouriteFruit.js
+++ FavouriteFruit.js
@@ -2,8 +2,21 @@ let favorite_fruits=["Orange","Banana","Apple","Pine Apple","Pears"];
 
 let certainFruit="Apple";
 
-for(let fruit of favorite_fruits){
-    if(fruit===certainFruit){
-        console.log(`I really like ${fruit}`)
-    }
+if(favorite_fruits[0]===certainFruit){
+    console.log(`I really like ${favorite_fruits[0]}`)
+}
+else if(favorite_fruits[1]===certainFruit){
+    console.log(`I really like ${favorite_fruits[1]}`)
+}
+else if(favorite_fruits[2]===certainFruit){
+    console.log(`I really like ${favorite_fruits[2]}`)
+}
+else if(favorite_fruits[3]===certainFruit){
+    console.log(`I really like ${favorite_fruits[3]}`)
+}
+else if(favorite_fruits[4]===certainFruit){
+    console.log(`I really like ${favorite_fruits[4]}`)
+}
+else{
+    console.log(`${certainFruit} is not in my favorite fruit list.`)
 }
